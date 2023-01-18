b318e2f74bb3c36612b970731299bb4735996ffd
T1me2 <jmiller10@css.edu>
2023-01-18 12:56:58 -0500

    final

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -1,6 +1,7 @@
 //read in the sample data
 const sample_data = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
+//make json data global variables
 let mydata;
 let names;
 let meta_data;
@@ -10,14 +11,16 @@ let id_tags = []
 //create function that updates plotly, pass it tracker so it updates with the first value in the dropdown b4 something is selected
 function updatePlotly(tracker) {
     
+    //filter by id
     let sample = samples.filter(obj => obj.id == tracker)[0]
         console.log(sample)
         
-
+        //define x axis y axis and labels for bar chart
         let bary = sample.otu_ids.slice(0,10).map(id => "OTU ID: " + id)
         let barx = sample.sample_values.slice(0,10)
         let bar_label = sample.otu_labels.slice(0,10)
 
+        //bar chart set up 
         bardata = [{
               x: barx,
               y: bary,
@@ -34,21 +37,19 @@ function updatePlotly(tracker) {
             
         }
         };
+
+        //define variables for bubble chart
         let bubblex = sample.otu_ids
         let bubbley = sample.sample_values
         let bubmarkersize = sample.sample_values
         let bubblecolor = sample.otu_ids
         let bubtext = sample.id
-        
+
+    //select the bubble chart based on id
     let bubmetadata = meta_data.filter(obj => obj.id == tracker)[0]
         console.log(bubmetadata)
-
-        console.log(bubblex)
-        console.log(bubbley)
-        console.log(bubmarkersize)
-        console.log(bubblecolor)
-        console.log(bubtext)
-
+        
+        //set up bubble chart
         bubble_data = [{
             x: bubblex,
             y: bubbley,
@@ -65,8 +66,20 @@ function updatePlotly(tracker) {
             showlegend: true
 
         };
-    
 
+        //define variables for demographic info
+        let ethnicity = bubmetadata.ethnicity
+        let gender = bubmetadata.gender
+        let age = bubmetadata.age
+        let location = bubmetadata.location
+        let bbtype = bubmetadata.bbtype
+        let wfreq = bubmetadata.wfreq
+
+        //insert demographic info
+        let demoinfo = ` id: ${tracker} <br> ethnicity: ${ethnicity} <br> gender: ${gender} <br> age: ${age} <br> location: ${location} <br> bbtype: ${bbtype} <br> wfreq: ${wfreq}`
+        document.getElementById('sample-metadata').innerHTML = demoinfo;
+    
+    //plot charts
     Plotly.newPlot("bar", bardata, barlayout);
     Plotly.newPlot("bubble", bubble_data, bubble_layout); 
     
@@ -75,7 +88,7 @@ function updatePlotly(tracker) {
         }
     
     
-
+//initialize the update plotly so the first id is shown when the page is loaded.
 function optionChanged(dropdownid) {
 
     
@@ -87,7 +100,7 @@ d3.json(sample_data).then(function(data){
     // build ddl of names
     names = data.names
     samples = data.samples
-    meta_data = data.meta_data
+    meta_data = data.metadata
     
     // isert id's into dropdown menu
     names.forEach(name => d3.select("#selDataset").append('option').text(name));
